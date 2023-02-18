0f653771cbe800e4d4b90da4fb83854bf8c1a618
Aristogiannis <aristogiannisphilippis@gmail.com>
2023-02-18 19:36:09 +0200

    Gps location working

--- app/src/main/java/load/ing/carefree/MainActivity.java
+++ app/src/main/java/load/ing/carefree/MainActivity.java
@@ -1,19 +1,26 @@
 package load.ing.carefree;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -23,31 +30,48 @@ public class MainActivity extends AppCompatActivity {
 
     private FusedLocationProviderClient fusedLocationClient;
 
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
+        LocationManager locationManager = null;
+        LocationListener locationListener = null;
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1 /*could be any number*/);
+        } else {
+            Toast permission_denied = Toast.makeText(getApplicationContext(), "Location permission not granted try again", Toast.LENGTH_LONG);
+            ;
+
+        }
 
         Button Signup = findViewById(R.id.signup);
         TextView location_view = findViewById(R.id.location);
-
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+            
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
 
-        Signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, List.class);
-                startActivity(intent);
-            }
-        });
-
-
-
-
+                            if (location != null) {
+                                double lat = location.getLatitude();
+                                double lon = location.getLongitude();
+                                String slon = Double.toString(lon);
+                                String slat = Double.toString(lat);
+                                location_view.setText(slat + "/" + slon);
+                            }
+                            Signup.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    Intent intent = new Intent(MainActivity.this, List.class);
+                                    startActivity(intent);
+                                }
+                            });
+                        }
+                    });
 
     }
 }
