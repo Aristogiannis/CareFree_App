d1eb3cc54b10a119f6018c41d35adc167de87757
Aristogiannis <aristogiannisphilippis@gmail.com>
2023-02-18 02:01:10 +0200

    Base application

---
+++ app/src/main/java/load/ing/carefree/List.java
@@ -0,0 +1,27 @@
+package load.ing.carefree;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+
+        EditText password = findViewById(R.id.password);
+        EditText email = findViewById(R.id.email);
+
+
+
+    }
+}
---
+++ app/src/main/java/load/ing/carefree/MainActivity.java
@@ -0,0 +1,53 @@
+package load.ing.carefree;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+
+        Button Signup = findViewById(R.id.signup);
+        TextView location_view = findViewById(R.id.location);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, List.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+    }
+}
