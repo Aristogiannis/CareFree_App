e5b6974fab17d18d0c3b9da809d7f087b9244d07
Aristogiannis <aristogiannisphilippis@gmail.com>
2023-02-20 01:56:43 +0200

    GPS tracking works, Get Directions works

--- app/src/main/java/load/ing/carefree/MainActivity.java
+++ app/src/main/java/load/ing/carefree/MainActivity.java
@@ -14,11 +14,14 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +31,23 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    static double haversine(double lat1, double lon1, double lat2, double lon2){
+
+         double dLat = Math.toRadians(lat2 - lat1);
+         double dLon = Math.toRadians(lon2 - lon1);
+         lat1 = Math.toRadians(lat1);
+         lat2 = Math.toRadians(lat2);
+         double a = Math.pow(Math.sin(dLat / 2), 2) +
+         Math.pow(Math.sin(dLon / 2), 2) *
+         Math.cos(lat1) *
+         Math.cos(lat2);
+         double rad = 6371.0072;
+         double c = 2 * Math.atan(Math.sqrt(a));
+         return rad * c;
+    }
+
+
     private FusedLocationProviderClient fusedLocationClient;
 
     @Override
@@ -36,21 +56,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
-        LocationManager locationManager = null;
-        LocationListener locationListener = null;
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1 /*could be any number*/);
-        } else {
-            Toast permission_denied = Toast.makeText(getApplicationContext(), "Location permission not granted try again", Toast.LENGTH_LONG);
-            ;
-
-        }
-
+        Double [] lats = {37.630238109521486, 37.62819300280989};
+        Double [] longs = {26.106339097445733, 26.158847526239356};
         Button Signup = findViewById(R.id.signup);
+        ImageButton get_directions = findViewById(R.id.get_directions);
         TextView location_view = findViewById(R.id.location);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1 );
+        } else {
+            Toast permission_denied = Toast.makeText(getApplicationContext(), "Location permission not granted try again", Toast.LENGTH_LONG);
+        }
             fusedLocationClient.getLastLocation()
                     .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                         @Override
@@ -59,13 +77,29 @@ public class MainActivity extends AppCompatActivity {
                             if (location != null) {
                                 double lat = location.getLatitude();
                                 double lon = location.getLongitude();
-                                String slon = Double.toString(lon);
-                                String slat = Double.toString(lat);
-                                location_view.setText(slat + "/" + slon);
+
+                               Double first = haversine(lat, lon,  lats[0], longs[0]);
+                               Double second= haversine(lat, lon,  lats[1], longs[1]);
+                               if (first < second){
+                                  location_view.setText(Double.toString(first) + " first");
+                                }
+                                else {location_view.setText(Double.toString(second));}
+
+
+
                             }
                         }
                     });
 
+        get_directions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=37.92073233218703, 23.711842658876936");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
 
         Signup.setOnClickListener(new View.OnClickListener() {
             @Override
