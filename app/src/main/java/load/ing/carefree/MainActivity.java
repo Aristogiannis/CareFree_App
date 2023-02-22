888c154415089a6be0875d64bf01a0c8322edafa
Aristogiannis <aristogiannisphilippis@gmail.com>
2023-02-22 20:44:48 +0200

    day save, original commit will be performed in about 2 hours

--- app/src/main/java/load/ing/carefree/MainActivity.java
+++ app/src/main/java/load/ing/carefree/MainActivity.java
@@ -1,27 +1,23 @@
 package load.ing.carefree;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,10 +25,14 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
+
 public class MainActivity extends AppCompatActivity {
 
 
-    static double haversine(double lat1, double lon1, double lat2, double lon2){
+    double haversine(double lat1, double lon1, double lat2, double lon2){
 
          double dLat = Math.toRadians(lat2 - lat1);
          double dLon = Math.toRadians(lon2 - lon1);
@@ -56,11 +56,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
-        Double [] lats = {37.630238109521486, 37.62819300280989};
-        Double [] longs = {26.106339097445733, 26.158847526239356};
+        Double [] pharmacy_lats =  {37.630238109521486, 37.62819300280989, 37.6307632419298, 37.6287162088105, 37.61314798821025, 37.615934428378075,37.92436740210267}; // last 2 cords in athens
+        Double [] pharmacy_longs = {26.106339097445733,26.158847526239356,26.178307223731224,26.18643483803518,26.294174906418764,26.295176613093464, 23.724148422567776};// last 2 cords in athens
+
+
         Button Signup = findViewById(R.id.signup);
-        ImageButton get_directions = findViewById(R.id.get_directions);
-        TextView location_view = findViewById(R.id.location);
+        ScrollView scrollView = findViewById(R.id.scrollView);
+        LinearLayout linearLayout = findViewById(R.id.linear);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
 
@@ -69,37 +71,55 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast permission_denied = Toast.makeText(getApplicationContext(), "Location permission not granted try again", Toast.LENGTH_LONG);
         }
+
             fusedLocationClient.getLastLocation()
                     .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @SuppressLint("SetTextI18n")
                         @Override
                         public void onSuccess(Location location) {
-
                             if (location != null) {
+                                int i;
                                 double lat = location.getLatitude();
                                 double lon = location.getLongitude();
+                                double [] distance = {0,0,0,0,0,0,0};
+                                //double min = 99999999;
 
-                               Double first = haversine(lat, lon,  lats[0], longs[0]);
-                               Double second= haversine(lat, lon,  lats[1], longs[1]);
-                               if (first < second){
-                                  location_view.setText(Double.toString(first) + " first");
+                                for(i = 0; i <= 6;i++) {distance[i] = haversine(lat, lon, pharmacy_lats[i], pharmacy_longs[i]);}
+
+                                for(i = 0;i<=6;i++){
+                                    int finalI = i;
+                                    int n = distance.length;
+                                    Arrays.sort(distance);
+
+                                    TextView textView = new TextView(MainActivity.this);
+                                    textView.setText("Pharmacy "+ (finalI+1)+" (" +new DecimalFormat("##.##").format(distance[i]) + " km)");
+                                    linearLayout.addView(textView);
+
+                                    ImageButton button = new ImageButton(MainActivity.this);
+                                    button.setImageResource(R.drawable.icons8_map_marker_48);
+                                    button.setBackground(null);
+
+                                    button.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            Uri gmmIntentUri = Uri.parse("geo:0,0?q="+Double.toString(pharmacy_lats[finalI]) +","+Double.toString(pharmacy_longs[finalI]));
+                                            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                                            mapIntent.setPackage("com.google.android.apps.maps");
+                                            startActivity(mapIntent);
+                                        }
+                                    });
+                                    linearLayout.addView(button);
+                                    linearLayout.setGravity(Gravity.RIGHT);
                                 }
-                                else {location_view.setText(Double.toString(second));}
-
 
 
                             }
+                            else{ Toast permission_denied = Toast.makeText(getApplicationContext(), "Failed to load location", Toast.LENGTH_LONG);}
                         }
                     });
 
-        get_directions.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri gmmIntentUri = Uri.parse("geo:0,0?q=37.92073233218703, 23.711842658876936");
-                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                mapIntent.setPackage("com.google.android.apps.maps");
-                startActivity(mapIntent);
-            }
-        });
+
+
 
         Signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -110,3 +130,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
