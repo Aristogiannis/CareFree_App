c99d95b93721dfa9d5085251ad64a481cd671072
Aristogiannis <aristogiannisphilippis@gmail.com>
2023-03-16 20:15:08 +0200

    final code

--- app/src/main/java/load/ing/carefree/List.java
+++ app/src/main/java/load/ing/carefree/List.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.LocationRequest;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class List extends AppCompatActivity {
@@ -18,8 +21,38 @@ public class List extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        EditText password = findViewById(R.id.password);
-        EditText email = findViewById(R.id.email);
+        ImageButton emergency_call = findViewById(R.id.emergency_call);
+        TextView emergency_text = findViewById(R.id.emergency_text);
+        ImageButton back_btn = findViewById(R.id.back__btn);
+        emergency_call.setBackground(null);
+        back_btn.setBackground(null);
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             Intent back = new Intent(List.this, MainActivity.class);
+             startActivity(back);
+            }
+        });
+
+
+        emergency_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent phone_intent = new Intent(Intent.ACTION_DIAL);
+                phone_intent.setData(Uri.parse("tel:" + "166"));
+                startActivity(phone_intent);
+            }
+        });
+
+        emergency_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent phone_intent = new Intent(Intent.ACTION_DIAL);
+                phone_intent.setData(Uri.parse("tel:" + "166"));
+                startActivity(phone_intent);
+            }
+        });
 
 
 
--- app/src/main/java/load/ing/carefree/MainActivity.java
+++ app/src/main/java/load/ing/carefree/MainActivity.java
@@ -15,6 +15,8 @@ import android.text.style.ForegroundColorSpan;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -23,6 +25,7 @@ import android.widget.RelativeLayout.LayoutParams;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -58,8 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     private FusedLocationProviderClient fusedLocationClient;
-    LocationRequest locationRequest;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,11 +68,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
+
+
+
         Double [] pharmacy_lats =  {37.630238109521486, 37.62819300280989, 37.6307632419298, 37.6287162088105, 37.61314798821025, 37.615934428378075,37.92436740210267}; // last 2 cords in athens
         Double [] pharmacy_longs = {26.106339097445733,26.158847526239356,26.178307223731224,26.18643483803518,26.294174906418764,26.295176613093464, 23.724148422567776};// last 2 cords in athens
-        String [] ratings = {"5/5","5/5","4/5","5/5","5/5","4.8/5", "69/420"};
-        String [] tel_numbers = {"0","1","2","3","4","5","6"};
-        Button Signup = findViewById(R.id.signup);
+        String [] ratings = {"5/5","5/5","4/5","5/5","5/5","4.8/5", "4/5"};
+        String [] tel_numbers = {"2275071271","2275032985","2275031394","4","2275023990","2275022212","7"};
+
+        ImageButton account = findViewById(R.id.account);
         ScrollView scrollView = findViewById(R.id.scrollView);
         LinearLayout linearLayout = findViewById(R.id.linear);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
@@ -81,7 +86,14 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(MainActivity.this, "Location permission not granted try again", Toast.LENGTH_LONG);
         }
-
+            account.setBackground(null);
+            account.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent acc_page = new Intent(MainActivity.this, List.class);
+                    startActivity(acc_page);
+                }
+            });
 
             fusedLocationClient.getLastLocation()
                     .addOnSuccessListener(this, new OnSuccessListener<Location>() {
@@ -92,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
                                 int i;
                                 double lat = location.getLatitude();
                                 double lon = location.getLongitude();
-                                double [] distance = {0,0,0,0,0,0,0};
+                                double [] distance = new double[pharmacy_lats.length];
                                 int len = distance.length;
 
                                 //TODO: AD COMMENTS ASAP OR COMMIT SUICIDE ON GITHUB
@@ -101,25 +113,34 @@ public class MainActivity extends AppCompatActivity {
                                 Arrays.sort(distance);
                                 reverseArray(distance);
 
-                                for(i = len-1;i >= 0;i--){
+                                for(i = len-1; i >= 0; i--){
 
                                     int finalI = i;
                                     TextView pharmacy_view = new TextView(MainActivity.this);
+                                    TextView no_num = new TextView(MainActivity.this);
+
+                                    no_num.setTextColor(Color.parseColor("#000000"));
+                                    no_num.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+                                    no_num.setText("Telephone Number is not available");
 
                                     pharmacy_view.setTextColor(Color.parseColor("#000000"));
                                     pharmacy_view.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+
                                     String pharmacy_string = ("Pharmacy "+ (len - finalI)+" (" +new DecimalFormat("##.##").format(distance[i]) + " km)  " + ratings[finalI] + " ★");
                                     SpannableString spannable = new SpannableString(pharmacy_string);
                                     spannable.setSpan(new ForegroundColorSpan(Color.parseColor("#FFD700")), pharmacy_string.length() - ratings[finalI].length()-2, (pharmacy_string).length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
                                     pharmacy_view.setText(spannable, TextView.BufferType.SPANNABLE);
-                                   // pharmacy_view.setBackgroundResource(R.drawable.backshape);
+
 
                                     ImageButton call_button = new ImageButton(MainActivity.this);
+
                                     call_button.setImageResource(R.drawable.icons8_phone_48);
                                     call_button.setBackground(null);
                                     call_button.setScaleX((float) 1);
                                     call_button.setScaleY((float) 1);
+
                                     ImageButton directions_button = new ImageButton(MainActivity.this);
+
                                     directions_button.setImageResource(R.drawable.icons8_map_marker_48);
                                     directions_button.setScaleX((float) 0.9);
                                     directions_button.setScaleY((float) 0.9);
@@ -144,6 +165,10 @@ public class MainActivity extends AppCompatActivity {
                                     });
 
                                     LinearLayout row = new LinearLayout(MainActivity.this);
+                                    LinearLayout back = new LinearLayout(MainActivity.this);
+                                    back.setOrientation(LinearLayout.VERTICAL);
+                                    back.setBackgroundResource(R.drawable.backshape);
+
                                     row.setOrientation(LinearLayout.HORIZONTAL);
 
                                     LinearLayout row2 = new LinearLayout(MainActivity.this);
@@ -156,7 +181,7 @@ public class MainActivity extends AppCompatActivity {
                                     );
                                     layoutParams.weight = -1;
                                     layoutParams.gravity = Gravity.LEFT;
-                                    layoutParams.setMargins(0,50,0,35);
+                                    layoutParams.setMargins(10,0,0,35);
 
 
                                     /** Layout parameters for get directions directions_button ***/
@@ -164,7 +189,7 @@ public class MainActivity extends AppCompatActivity {
                                             LinearLayout.LayoutParams.WRAP_CONTENT,
                                             LinearLayout.LayoutParams.WRAP_CONTENT
                                     );
-                                    layoutParams_btn.gravity = Gravity.RIGHT;
+                                    layoutParams_btn.gravity = Gravity.LEFT;
                                     layoutParams_btn.setMargins(0,50,0,0);
                                     layoutParams_btn.weight =-1;
 
@@ -178,47 +203,40 @@ public class MainActivity extends AppCompatActivity {
                                     row_params.setMargins(45,50,0,0);
 
                                     /** Layout parameters for rows ***/
-                                    LinearLayout.LayoutParams row2_params = new LinearLayout.LayoutParams(
-                                            LinearLayout.LayoutParams.WRAP_CONTENT,
-                                            LinearLayout.LayoutParams.WRAP_CONTENT
-                                    );
-
-
+                                    LinearLayout.LayoutParams back_params = new LinearLayout.LayoutParams(
+                                            LinearLayout.LayoutParams.MATCH_PARENT,
+                                            LinearLayout.LayoutParams.MATCH_PARENT);
+                                    back_params.setMargins(30,50,30,0);
 
 
                                     pharmacy_view.setLayoutParams(layoutParams);
                                     directions_button.setLayoutParams(layoutParams_btn);
                                     row.setLayoutParams(row_params);
-                                    row.setBackgroundResource(R.drawable.backshape);
-                                    //row2.setBackgroundColor(Color.parseColor("#bfdcfc"));
+                                    back.setLayoutParams(back_params);
                                     row.addView(pharmacy_view);
                                     row2.addView(directions_button);
-                                    row2.addView(call_button);
-                                    linearLayout.addView(row);
-                                    linearLayout.addView(row2);
 
+
+                                    if (tel_numbers[finalI].length() < 9){
+                                        row2.addView(no_num);
+                                    }
+                                    else{
+                                        row2.addView(call_button);
+                                    }
+
+                                    back.addView(row);
+                                    back.addView(row2);
+                                    linearLayout.addView(back);
                                 }
 
 
                             }
-                            else{Toast.makeText(MainActivity.this, "Failed to load location", Toast.LENGTH_LONG);}
+                            //else{Toast.makeText(MainActivity.this, "Failed to load location", Toast.LENGTH_LONG);}
                         }
                     });
-
-
-
-
-        Signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, List.class);
-                startActivity(intent);
-            }
-        });
     }
 
-
-    public static void reverseArray(double[] arr) {
+    public static void reverseArray(double[] arr){
         int start = 0;
         int end = arr.length - 1;
         while (start < end) {
